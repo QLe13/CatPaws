b3c4152b5e737f25cc8a084a8a0f420a86d68db7
Quang <qle13@Qs-MacBook-Air-4.local>
2023-03-01 12:30:14 -0600

    Complete Routing

--- client/src/components/Navbar/NavBar.jsx
+++ client/src/components/Navbar/NavBar.jsx
@@ -13,8 +13,8 @@ function NavBar(props) {
       <ul className="navbar__menu">
         <li className="navbar__menu-item"><a href="/">User Info</a></li>
         <li className="navbar__menu-item"><a href="/">Schedule</a></li>
-        <li className="navbar__menu-item"><a href="#">Registration</a></li>
-        <li className="navbar__menu-item"><a href="#">Find Classes</a></li>
+        <li className="navbar__menu-item"><a href="/register">Registration</a></li>
+        <li className="navbar__menu-item"><a href="/findclasses">Find Classes</a></li>
         <li className="navbar__menu-item"><a href="/" onClick={()=>deleteCookies(props.cookies)}>Log out</a></li> 
         {/* We can make the log out button double confirmation about logging out later on */}
       </ul>
