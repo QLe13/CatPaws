fac930b477a05e4af0c87e85dae49627bd38821c
Joshua Gammon <joshuagammon@joshuas-air.trinity.local>
2023-04-06 15:54:25 -0500

    Navbar edits

--- client/src/components/Navbar/NavBar.jsx
+++ client/src/components/Navbar/NavBar.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import './nav.css';
 
-const NavBar=({cookies, isTeacher})=> {
-  const deleteCookies = (cookies) =>{
+const NavBar = ({ cookies, isTeacher }) => {
+  const deleteCookies = (cookies) => {
     cookies.remove('curUser')
-  }
+  };
+  
   return (
     <nav className="navbar">
       <ul className="navbar__menu">
         <li className="navbar__menu-item">
-          <a href="/" >User Info</a>
+          <a href="/">User Info</a>
         </li>
         <li className="navbar__menu-item">
           <a href="/schedule">Schedule</a>
         </li>
         <li className="navbar__menu-item">
-          <a href={!isTeacher?"/findclasses":"createclasses"}>{!isTeacher?"Find Classes":"Create Classes"}</a>
+          <a href={!isTeacher ? "/findclasses" : "createclasses"}>{!isTeacher ? "Find Classes" : "Create Classes"}</a>
         </li>
         <li className="navbar__menu-item">
           <a href="/register">Registration</a>
         </li>
         <li className="navbar__menu-item">
           <a href="/" onClick={() => deleteCookies(cookies)}>Log out</a>
-        </li> 
-        {/* We can make the log out button double confirmation about logging out later on */}
+        </li>
       </ul>
     </nav>
   );
