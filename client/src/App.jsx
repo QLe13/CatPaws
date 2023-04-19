6bf97411bb7f9770e797c1c8c5c198cabd5eedb4
lwarden1 <lwarden1@trinity.edu>
2023-04-18 21:20:43 -0500

    start firebase setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState} from 'react'
+import React, { useState } from 'react'
 import Landing from './components/Landing/Landing'
 import Register from './components/Register/Register'
 import FindClasses from './components/FindClasses/FindClasses'
@@ -6,38 +6,36 @@ import Cookies from 'universal-cookie'
 import NavBar from './components/Navbar/NavBar'
 import Auth from './components/Landing/Auth'
 import CreateClasses from './components/CreateClasses/CreateClasses'
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 const cookies = new Cookies()
-const App = () => {
-  const [curUser, setUser] = useState(cookies.get('curUser')||null)// This is where the user information object is being stored
+const App = ({ curUser }) => {
+  const [curUser, setUser] = useState(cookies.get('curUser') || null)// This is where the user information object is being stored
   const setUserState = (newUser) => {
     setUser(newUser);
-    cookies.set('curUser',newUser)//This is the user object from the backend
+    cookies.set('curUser', newUser)//This is the user object from the backend
     //this is the function that sets the user state and sets the cookie
-  }; 
+  };
 
-  if(!curUser){
-    return <Auth setUser ={setUserState}/>
+  if (!curUser) {
+    return <Auth setUser={setUserState} />
     // What does this conditional do? It validates the user and if the user is not logged in, it will redirect them to the login page
-}
+  }
   return (
     <div>
-  <Router>
-      <NavBar cookies = {cookies} isTeacher={curUser.data.isTeacher}/>
-      <Routes>
-        <Route exact path='/' element={<Landing user = {curUser}/>} />
-        <Route exact path='/schedule' element={<Landing user = {curUser}/>}/>
-        <Route exact path='/register' element={<Register user = {curUser}/>}/>
-        <Route exact path='/createclasses' element={ <CreateClasses/> }/>
-        <Route exact path='/findclasses' element={<FindClasses user = {curUser}/>}/>
-      </Routes>
-  </Router>
+      <Router>
+        <NavBar cookies={cookies} isTeacher={curUser.data.isTeacher} />
+        <Routes>
+          <Route exact path='/' element={<Landing user={curUser} />} />
+          <Route exact path='/schedule' element={<Landing user={curUser} />} />
+          <Route exact path='/register' element={<Register user={curUser} />} />
+          <Route exact path='/createclasses' element={<CreateClasses />} />
+          <Route exact path='/findclasses' element={<FindClasses user={curUser} />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
 
 export default App
-
-
--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,16 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
-import { getAuth, onAuthStateChanged, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, onAuthStateChanged, GoogleAuthProvider, EmailAuthProvider } from "firebase/auth";
 
-export const provider = new GoogleAuthProvider();
-provider.addScope('https://www.googleapis.com/auth/calendar');
-provider.addScope('https://www.googleapis.com/auth/gmail.insert');
+export const googleProvider = new GoogleAuthProvider();
+googleProvider.addScope('https://www.googleapis.com/auth/calendar');
+googleProvider.addScope('https://www.googleapis.com/auth/gmail.insert');
 // more scopes... e.g. classroom.courses ... reference: https://developers.google.com/identity/protocols/oauth2/scopes?authuser=0
-provider.setCustomParameters({
+googleProvider.setCustomParameters({
     'login_hint': 'user@trinity.edu'
 });
+
+export const emailProvider = new EmailAuthProvider();
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -29,21 +31,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
+// export const analytics = getAnalytics(app);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-
-onAuthStateChanged(auth, (user) => {
-    if (user) {
-        // User is signed in, see docs for a list of available properties
-        // https://firebase.google.com/docs/reference/js/firebase.User
-        const uid = user.uid;
-        // ...
-    } else {
-        // User is signed out
-        // ...
-    }
-});
 
 // docs setup: https://firebase.google.com/docs/web/setup
 // firebase rules: https://firebase.google.com/docs/rules/basics
